af1505c32eeb32eb23ca91c82501b02c798af8fb
Arman Jon Villalobos <me@armanjon.com>
2017-07-29 16:15:11 +0800

    Add a success page to tell what user ids we approve

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   
-  @@token = "75a962dd1f0e3bdae3abc5ef4bcb91fa41ee7f1b43f87de9dca575cee640d692"
+  @@token = "62dd0a9e31913c9524c2c065798b2ce368515f17df777eeb843e96dffaacce6e"
   @@header = {"Authorization" => "Bearer #{@@token}", "X-Tanda-Audit-Name" => "Gazza"}
   def index
   end
@@ -15,41 +15,40 @@ class DashboardController < ApplicationController
       @current_users << timesheet["user_id"]
     end
     check_timesheet(@current_users)
+    flash[:ids] = @current_users
+    redirect_to success_path
   end
   
   def check_timesheet(user_list)
     user_list.each do |user|
       approve(user)
     end
-    redirect_to 'https://staging.tanda.co/timesheets'
   end
   
   def approve(user)
     user_id = user
     url = "https://staging.tanda.co/api/v2/timesheets/for/#{user_id}/current?show_costs=true"
-    
     response = HTTParty.get(url, headers: @@header).parsed_response
 
     to_be_approved_shifts = []
-    
-    response["shifts"].each do |shift|
-      schedule_url = "https://staging.tanda.co/api/v2/schedules?user_ids=#{shift["user_id"]}&from=#{shift["date"]}&to=#{shift["date"]}&show_costs=true&include_names=false" 
-      schedule_response = HTTParty.get(schedule_url, headers: @@header).parsed_response.first
-      unless schedule_response.nil?
-      
-      to_be_approved_shifts << shift if schedule_response["cost"] == shift["cost"]
-      end
-    
-    to_be_approved_shifts.each do |shift|
-      shift_approved_url = "https://staging.tanda.co/api/v2/shifts/#{shift["id"]}"
-      body = { status: "APPROVED" }
-      
-      HTTParty.put(shift_approved_url, headers: @@header, body: body)
-    end
-    end
-    
-  
-  end
-    
 
+    unless response["shifts"].nil?
+      response["shifts"].each do |shift|
+        schedule_url = "https://staging.tanda.co/api/v2/schedules?user_ids=#{shift["user_id"]}&from=#{shift["date"]}&to=#{shift["date"]}&show_costs=true&include_names=false" 
+        schedule_response = HTTParty.get(schedule_url, headers: @@header).parsed_response.first
+        
+        unless schedule_response.nil?
+          to_be_approved_shifts << shift if schedule_response["cost"] == shift["cost"]
+          to_be_approved_shifts.each do |shift|
+            shift_approved_url = "https://staging.tanda.co/api/v2/shifts/#{shift["id"]}"
+            body = { status: "APPROVED" }
+            HTTParty.put(shift_approved_url, headers: @@header, body: body)
+          end
+        end
+      end
+    end
+  end
+
+  def success
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'approve', to: 'dashboard#approve' 
   get 'approve_shifts', to: 'dashboard#approve_shifts'
   get 'find_users_with_timesheets', to: 'dashboard#find_users_with_timesheets'
+  get 'success', to: 'dashboard#success'
 end
